f443459fb8a6c02c3ec0932f6511e4ed39660513
Derek Fitzer <franzderek@gmail.com>
2025-04-28 14:44:58 -0400

    Initial Commit

---
+++ APPSIMU/APPSIMUApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPSIMUApp.swift
+//  APPSIMU
+//
+//  Created by Derek Fitzer on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct APPSIMUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
